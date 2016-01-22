fe3fbca94709a5bd0876c9831f7a242b13932d24
OperKH <oper.kh@gmail.com>
2016-01-22 16:33:01 +0200

    update because UCRF has problems with their db.

--- cron_scripts/updateData.js
+++ cron_scripts/updateData.js
@@ -11,51 +11,49 @@ var outputIndexHtml = frontendDir + 'index.html';
 
 console.log('before responce');
 
-requestify.get('http://www.ucrf.gov.ua/wp-admin/admin-ajax.php?action=get_wdtable&table_id=1&sEcho=1&sSearch_9=UMTS&bSearchable_9=true').then(function(response) {
+requestify.get('http://www.ucrf.gov.ua/wp-admin/admin-ajax.php?action=get_wdtable&table_id=1&sEcho=1&sSearch_0=БС2100-&bSearchable_0=true').then(function(response) {
     console.log('get responce');
     var data = JSON.parse(response.getBody()).aaData;
     var mainData = {};
     data.forEach(function(item){
-        if (item[0]) {
-            var date = new Date(item[1].split("/").reverse().join("/"));
-            var province = item[3];
-            var city = item[4];
-            var cityKey = city + '_' + province;
-            var equipmentBrand = item[5];
-            var freq = item[7];
-            var operatorNameKey = /1922|1927|1932/i.test(freq)?"life":/1937|1942|1947/i.test(freq)?"triMob":/1952|1957|1962/i.test(freq)?"mts":/1967|1972|1977/i.test(freq)?"ks":freq;
-            equipmentBrand = /RBS 3206|RBS6102|RBS6201|RBS6302|RBS6000|RBS6601/i.test(equipmentBrand)?"Ericsson":/Nokia|Flexi Multiradio/i.test(equipmentBrand)?"Nokia":/BTS 3803|DBS 3800|BTS3812|BTS 3900|DBS\s?3900/i.test(equipmentBrand)?"Huawei":/ZXSDR BS8700/i.test(equipmentBrand)?"ZTE":/MobileAccess GX/i.test(equipmentBrand)?"Corning":equipmentBrand;
-            if (typeof(mainData[operatorNameKey]) === "undefined") {
-                mainData[operatorNameKey] = {};
-                mainData[operatorNameKey].provinces = {};
-                mainData[operatorNameKey].cities = {};
-                mainData[operatorNameKey].total = 0;
-            }
-            if (typeof(mainData[operatorNameKey].provinces[province]) === "undefined") {
-                mainData[operatorNameKey].provinces[province] = {};
-                mainData[operatorNameKey].provinces[province].date = date;
-                mainData[operatorNameKey].provinces[province].quantity = 0;
-                mainData[operatorNameKey].provinces[province].brand = {};
-            }
-            if (typeof(mainData[operatorNameKey].cities[cityKey]) === "undefined") {
-                mainData[operatorNameKey].cities[cityKey] = {};
-                mainData[operatorNameKey].cities[cityKey].city = city;
-                mainData[operatorNameKey].cities[cityKey].province = province;
-                mainData[operatorNameKey].cities[cityKey].date = date;
-                mainData[operatorNameKey].cities[cityKey].quantity = 0;
-                mainData[operatorNameKey].cities[cityKey].brand = {};
-            }
-            mainData[operatorNameKey].total++;
-
-            mainData[operatorNameKey].provinces[province].date = new Date(Math.max(mainData[operatorNameKey].provinces[province].date, date));
-            mainData[operatorNameKey].provinces[province].quantity++;
-            mainData[operatorNameKey].provinces[province].brand[equipmentBrand] = mainData[operatorNameKey].provinces[province].brand[equipmentBrand]?mainData[operatorNameKey].provinces[province].brand[equipmentBrand]+1:1;
-
-            mainData[operatorNameKey].cities[cityKey].date = new Date(Math.max(mainData[operatorNameKey].cities[cityKey].date, date));
-            mainData[operatorNameKey].cities[cityKey].quantity++;
-            mainData[operatorNameKey].cities[cityKey].brand[equipmentBrand] = mainData[operatorNameKey].cities[cityKey].brand[equipmentBrand]?mainData[operatorNameKey].cities[cityKey].brand[equipmentBrand]+1:1;
-
+        var date = new Date(item[1].split("/").reverse().join("/"));
+        var province = item[3];
+        var city = item[4];
+        var cityKey = city + '_' + province;
+        var equipmentBrand = item[5];
+        var freq = item[7];
+        var operatorNameKey = /1922|1927|1932/i.test(freq)?"life":/1937|1942|1947/i.test(freq)?"triMob":/1952|1957|1962/i.test(freq)?"mts":/1967|1972|1977/i.test(freq)?"ks":freq;
+        equipmentBrand = /RBS 3206|RBS6102|RBS6201|RBS6302|RBS6000|RBS6601/i.test(equipmentBrand)?"Ericsson":/Nokia|Flexi Multiradio/i.test(equipmentBrand)?"Nokia":/BTS 3803|DBS 3800|BTS3812|BTS 3900|DBS\s?3900/i.test(equipmentBrand)?"Huawei":/ZXSDR BS8700/i.test(equipmentBrand)?"ZTE":/MobileAccess GX/i.test(equipmentBrand)?"Corning":equipmentBrand;
+        if (typeof(mainData[operatorNameKey]) === "undefined") {
+            mainData[operatorNameKey] = {};
+            mainData[operatorNameKey].provinces = {};
+            mainData[operatorNameKey].cities = {};
+            mainData[operatorNameKey].total = 0;
         }
+        if (typeof(mainData[operatorNameKey].provinces[province]) === "undefined") {
+            mainData[operatorNameKey].provinces[province] = {};
+            mainData[operatorNameKey].provinces[province].date = date;
+            mainData[operatorNameKey].provinces[province].quantity = 0;
+            mainData[operatorNameKey].provinces[province].brand = {};
+        }
+        if (typeof(mainData[operatorNameKey].cities[cityKey]) === "undefined") {
+            mainData[operatorNameKey].cities[cityKey] = {};
+            mainData[operatorNameKey].cities[cityKey].city = city;
+            mainData[operatorNameKey].cities[cityKey].province = province;
+            mainData[operatorNameKey].cities[cityKey].date = date;
+            mainData[operatorNameKey].cities[cityKey].quantity = 0;
+            mainData[operatorNameKey].cities[cityKey].brand = {};
+        }
+        mainData[operatorNameKey].total++;
+
+        mainData[operatorNameKey].provinces[province].date = new Date(Math.max(mainData[operatorNameKey].provinces[province].date, date));
+        mainData[operatorNameKey].provinces[province].quantity++;
+        mainData[operatorNameKey].provinces[province].brand[equipmentBrand] = mainData[operatorNameKey].provinces[province].brand[equipmentBrand]?mainData[operatorNameKey].provinces[province].brand[equipmentBrand]+1:1;
+
+        mainData[operatorNameKey].cities[cityKey].date = new Date(Math.max(mainData[operatorNameKey].cities[cityKey].date, date));
+        mainData[operatorNameKey].cities[cityKey].quantity++;
+        mainData[operatorNameKey].cities[cityKey].brand[equipmentBrand] = mainData[operatorNameKey].cities[cityKey].brand[equipmentBrand]?mainData[operatorNameKey].cities[cityKey].brand[equipmentBrand]+1:1;
+
     });
 
 
