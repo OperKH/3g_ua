85ed31a935d15ac506f0e069459e11a38575a266
OperKH <oper.kh@gmail.com>
2016-09-28 16:46:17 +0300

    Fix formatting.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,25 +1,26 @@
-$(document).ready(function(){
+$(document).ready(function() {
     // define sugar.js Icelandic sort order
     Array.AlphanumericSortOrder = 'абвгґдеєжзиіїйклмнопрстуфхцчшщюяь';
     Array.AlphanumericSortIgnoreCase = true;
     // see https://github.com/andrewplummer/Sugar/issues/382#issuecomment-41526957
     Array.AlphanumericSortEquivalents = {};
 
-    $('.panel-collapsible').find('.panel-heading').on('click',function(){
-        $(this).parent().toggleClass('panel-collapse');
-        $(this).next().slideToggle(500);
+    $('.panel-collapsible').find('.panel-heading').on('click', function() {
+        $(this)
+            .parent().toggleClass('panel-collapse').end()
+            .next().slideToggle(500);
     });
 
     $('table').tablesorter({
-        dateFormat: "ddmmyyyy",
-        widgets: ["filter"],
-        widgetOptions : {
+        dateFormat: 'ddmmyyyy',
+        widgets: ['filter'],
+        widgetOptions: {
             filter_placeholder: {
                 search: 'пошук...'
             }
         },
-        textSorter : {
-            0 : Array.AlphanumericSort
+        textSorter: {
+            0: Array.AlphanumericSort
         }
     });
 
